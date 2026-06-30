486b45bb88fc048e7c4e6b178de615418c40959f
ptsarfo <ptsarfo@st.ug.edu.gh>
2026-06-30 13:46:01 +0000

    Switch to Neon serverless driver for Vercel

--- database.js
+++ database.js
@@ -1,8 +1,7 @@
-const { Pool } = require('pg');
+const { Pool } = require('@neondatabase/serverless');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
 async function init() {
